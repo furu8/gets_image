e29fe1be157602a8065b68bcc33884757e80248a
Naoki Furuhama <foooolurk1125@gmail.com>
2018-08-23 16:51:23 +0900

    .gif以外を取得

--- image_get.py
+++ image_get.py
@@ -1,20 +1,25 @@
+import time
 import requests
 from requests.compat import urljoin
 from bs4 import BeautifulSoup
 
-URL = 'http://re-zero-anime.jp/'
+URL = 'https://www.kagawa-u.ac.jp/kagawa-u_eng/'
+images= []
+
 def download_images(url):
     soup = BeautifulSoup(requests.get(url).content, 'lxml')
     for link in soup.find_all("img"):
         src_attr = link.get("src")
-        print(src_attr)
-        # 相対URLから絶対URLに変換
-        yield urljoin(URL, src_attr)
+        if not src_attr.endswith(".gif"):
+            print(src_attr)
+            # 相対URLから絶対URLに変換
+            yield urljoin(URL, src_attr)
 
 def main():
     for target in download_images(URL):
         re = requests.get(target)
-        with open('img/' + target.split('/')[-1], 'wb') as f:
+        time.sleep(1)
+        with open('img/' + target.split('/')[-1], 'wb') as f: # splitでファイル名を短縮する
             f.write(re.content)
 
 if __name__ == '__main__':
